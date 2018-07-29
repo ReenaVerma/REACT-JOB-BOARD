c585a9e113d291f9d21a0fde37c3d2d3fdf76824
ReenaVerma <reena_verma@hotmail.com>
2018-07-29 18:16:28 +0100

    axios request working

--- src/app.js
+++ src/app.js
@@ -1,21 +1,40 @@
 import React from 'react';
+import axios from 'axios';
 import ReactDOM from 'react-dom';
-// import Navbar from './components/Navbar';
 import Header from './components/Header';
 import './assets/scss/main.scss';
 
 class App extends React.Component {
 
-
   constructor() {
     super();
+    console.log('CONSTRUCTOR');
 
     this.state = {
+      jobs: []
     };
   }
 
+
+  // MAKE JOB API REQUEST
+  componentDidMount() {
+    console.log('COMPONENT DID MOUNT');
+
+    axios.get('http://api.giphy.com/v1/gifs/search?q=trending&api_key=qj6zOrb0GLOPg317P3uw6LrWevvWFy5Q')
+
+      .then(res => {
+        console.log('jobs', res);
+
+        this.setState(
+          { jobs: res }
+        );
+
+      });
+  }
+
+
   render() {
-    return (
+    return(
       <main>
         <section>
           {/* <Navbar /> */}
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Header = () => (
 
   <section className='hero homepage'>
-
     <div className='container'>
       <h1 className='title has-text-white'>USA Creative City</h1>
       <h2 className='title has-text-white'>Start your job search here!</h2>
